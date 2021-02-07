536fbb05600458baba0ff43b44d81346b93166ce
peterbaile <peterchenwflms@gmail.com>
2021-02-07 18:28:27 -0500

    homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,150 +1,275 @@
-import * as React from "react"
+import React from 'react'
+import s from 'styled-components'
+import { Link } from 'gatsby'
+import { Helmet } from 'react-helmet'
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: "300",
-  fontSize: "24px",
-  maxWidth: "560px",
-}
+import projectsJSON from '../content/json/projects.json'
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: "16px",
-  verticalAlign: "5%",
-}
+const Wrapper = s.div`
+  body {
+    font-family: 'pt-serif', serif;
+    font-style: normal;
+    margin: 0;
+  }
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
+  nav img {
+    width: 125px;
+    display: block;
+    margin: auto;
+    padding: 15px 0;
+  }
 
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: "14px",
-}
+  h1, h2 {
+    margin: 85px 0 10px;
+    font-family: 'futura-pt', sans-serif;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 72px;
+    text-align: center;
+  }
 
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#000000",
-  },
-]
+  h1 {
+    color: #4BBFBE;
+  }
 
-// markup
-const IndexPage = () => {
+  h2 {
+    margin: 20px 0 25px;
+    font-size: 50px;
+  }
+
+  hr {
+    margin: 35px 0;
+  }
+
+  a, a:visited {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  a:hover, a:active, a:focus {
+    text-decoration: none;
+    color: #4BBFBE;
+  }
+
+  footer {
+    margin: 20px auto;
+    text-align: center;
+  }
+
+  footer hr {
+    margin-bottom: 20px;
+  }
+
+  .container {
+    margin: 0 auto;
+  }
+
+  .row {
+    margin: 0;
+  }
+
+  .article-row {
+    display: flex;
+    align-items: flex-end;
+  }
+
+  .featured-row {
+    padding: 0;
+  }
+
+  .featured {
+    padding: 15px 15px 0;
+  }
+
+  .top:first-of-type {
+    border-right: 1px solid #EEE;
+    padding-right: 40px;
+  }
+
+  .top:last-of-type {
+    padding-left: 40px;
+  }
+
+  .info {
+    font-family: "futura-pt", sans-serif;
+    text-transform: uppercase;
+    color: #999;
+    font-size: 20px;
+    padding-bottom: 0;
+  }
+
+  .tag {
+    font-weight: 700;
+    padding-left: 0;
+  }
+
+  .date {
+    font-weight: 400;
+    text-align: right;
+    padding-right: 0;
+  }
+
+  .top-tag, .top-date {
+    padding-top: 15px;
+  }
+
+  .title {
+    font-family: "futura-pt", sans-serif;
+    color: #000;
+    font-weight: 500;
+    font-size: 40px;
+    line-height: 1.2;
+  }
+
+  .featured-title {
+    font-weight: 700;
+    font-size: 38px;
+    padding-bottom: 15px;
+  }
+
+  .description {
+    font-size: 20px;
+  }
+
+  .credits {
+    color: #999;
+    font-size: 15px;
+    padding-top: 15px;
+  }
+
+  ::selection {
+    background: #4BBFBE;
+    color: #FFF;
+  }
+
+  @media screen and (max-width: 991px) {
+    hr {
+      margin: 30px 0;
+    }
+
+    .article-row {
+      flex-direction: column;
+    }
+
+    .article-info {
+      padding-top: 15px;
+    }
+
+    .top:first-of-type {
+      border-right: none;
+      border-bottom: 1px solid #EEE;
+      padding: 0 15px 30px;
+    }
+
+    .top:last-of-type {
+      padding: 30px 15px 0;
+    }
+  }
+`
+
+const URL = ({ link, children }) => {
+  if (link.includes('http'))
+    return (
+      <a href={link} target="_blank" rel="noreferrer">
+        {children}
+      </a>
+    )
   return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
+    <Link to={link} target="_blank" rel="noreferrer">
+      {children}
+    </Link>
   )
 }
 
-export default IndexPage
+const Article = ({
+  image,
+  alt,
+  section,
+  date,
+  description,
+  sponsored,
+  author,
+  link,
+  title
+}) => (
+  <>
+    <div className="row article-row">
+      <div className="col-md-4">
+        <img src={image} className="img img-responsive" alt={alt} />
+      </div>
+      <div className="col-md-8 article-info">
+        <div className="row info">
+          <div className="col-xs-6 tag">{section}</div>
+          <div className="col-xs-6 date">{date}</div>
+        </div>
+        <div className="row title">
+          <a href={link} target="_blank" rel="noreferrer">
+            {title}
+          </a>
+        </div>
+        <div className="row description">{description}</div>
+        <div className="row credits">
+          {sponsored ? `Sponsored by ${author}` : author}
+        </div>
+      </div>
+    </div>
+    <hr />
+  </>
+)
+
+const Index = () => (
+  <Wrapper>
+    <Helmet>
+      <meta charset="utf-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>Projects | 34th Street</title>
+      <meta
+        name="description"
+        content="This site showcases the work done by the DP's web development department, such as project pages for special issues or standalone interactives"
+      />
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css"
+      />
+    </Helmet>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <a href="https://www.34st.com" target="_blank">
+        <img
+          src="https://snworksceo.imgix.net/dpn-34s/f565f073-350e-4a86-aee9-1d2a3f3798ac.sized-1000x1000.png?"
+          alt="34th Street"
+        />
+      </a>
+    </nav>
+
+    <div class="container">
+      <h1>PROJECTS &amp; GUIDES</h1>
+
+      <div class="row article-row featured-row" id="featured-main"></div>
+
+      <hr />
+
+      <div class="row" id="featured-secondary"></div>
+
+      <hr />
+
+      <h2>All Projects</h2>
+
+      <div id="articles">
+        {projectsJSON.articles.map(article => (
+          <Article {...article} />
+        ))}
+      </div>
+
+      <footer>
+        <hr />
+        <p>
+          Copyright &copy; 2018, The Daily Pennsylvanian. All Rights Reserved.
+        </p>
+      </footer>
+    </div>
+  </Wrapper>
+)
+
+export default Index

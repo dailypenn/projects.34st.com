5b0c46b3e38f774da0bd0c73973145c1889ee166
peterbaile <peterchenwflms@gmail.com>
2021-02-08 18:06:33 -0500

    update analytics tracking code

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: 'UA-707447-4',
+        trackingId: 'UA-188534367-2',
       },
     },
     // images for 2021/love-issue

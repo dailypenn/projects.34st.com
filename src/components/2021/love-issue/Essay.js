524dbc661e27da75ea98345f76a625fd9093ab5c
peterbaile <peterchenwflms@gmail.com>
2021-02-10 18:21:45 -0500

    add nassim todos

--- src/components/2021/love-issue/Essay.js
+++ src/components/2021/love-issue/Essay.js
@@ -1,11 +1,9 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Img from 'gatsby-image'
-//import Carousel from 'react-bootstrap/Carousel'
-import {Container, Tag, Author, LoveHeading} from '../../shared/index.js'
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
+import Carousel from 'react-multi-carousel'
+import 'react-multi-carousel/lib/styles.css'
 
-
+import { Container, Tag, Author, LoveHeading } from '../../shared/index.js'
 
 const responsive = {
   desktop: {
@@ -23,64 +21,60 @@ const responsive = {
     items: 1,
     slidesToSlide: 1 // optional, default to 1.
   }
-};
-
-
-class EssayCarousel extends Component {
-  constructor(props) {
-    super(props);
- 
-    this.state = {
-      isShow: true,
-		};
-
-  }
- 
-  toggleShow = () => {
-    this.setState(state => ({ isShow: !state.isShow }));
-	};
-	
-  render() {
-    return (
-			<>
-				<div style={{ margin: '0 2rem' }}>
-					<LoveHeading color='#FDB6B0'>{'ESSAY WINNERS'}</LoveHeading>
-				</div>
-				<Carousel
-					swipeable={false}
-					draggable={false}
-					showDots={true}
-					responsive={responsive}
-					ssr={true} // means to render carousel on server-side.
-					infinite={true}
-					autoPlay={this.props.deviceType !== "mobile" ? true : false}
-					autoPlaySpeed={3500}
-					keyBoardControl={true}
-					customTransition="all .5"
-					transitionDuration={500}
-					containerClass="carousel-container"
-					removeArrowOnDeviceType={["tablet", "mobile"]}
-					deviceType={this.props.deviceType}
-					dotListClass="custom-dot-list-style"
-					itemClass="carousel-item-padding-40-px"
-				>
-					{this.props.articles &&
-					this.props.articles.map(article => (  
-					<Container>
-						<Tag>
-							<p style={{marginBottom: "0.2rem"}}>{article.title}</p>
-							<Author>BY {article.author}</Author>
-						</Tag>
-						<Img fluid={article.img.childImageSharp.fluid}/>
-					</Container>
-					))}
-					
-			</Carousel>
-		</>
-    );
-  }
 }
 
+// Nassim TODO: change this to functional component
+// follow the wireframe and add the 1st, 2nd, and 3rd badges
+// u can refer to this code https://github.com/dailypenn/tech.thedp.com/blob/master/src/components/badge.js
+class EssayCarousel extends Component {
+  constructor(props) {
+    super(props)
 
+    this.state = {
+      isShow: true
+    }
+  }
+
+  toggleShow = () => {
+    this.setState(state => ({ isShow: !state.isShow }))
+  }
+
+  render() {
+    return (
+      <>
+        <div style={{ margin: '0 2rem' }}>
+          <LoveHeading color="#FDB6B0">{'ESSAY WINNERS'}</LoveHeading>
+        </div>
+        <Carousel
+          draggable={false}
+          showDots={true}
+          responsive={responsive}
+          ssr={true} // means to render carousel on server-side.
+          infinite={true}
+          autoPlay={this.props.deviceType !== 'mobile' ? true : false}
+          autoPlaySpeed={3500}
+          keyBoardControl={true}
+          customTransition="all .5"
+          transitionDuration={500}
+          containerClass="carousel-container"
+          deviceType={this.props.deviceType}
+          dotListClass="custom-dot-list-style"
+          itemClass="carousel-item-padding-40-px"
+        >
+          {this.props.articles &&
+            this.props.articles.map(article => (
+              <Container>
+                <Tag>
+                  <p style={{ marginBottom: '0.2rem' }}>{article.title}</p>
+                  <Author>BY {article.author}</Author>
+                </Tag>
+                <Img fluid={article.img.childImageSharp.fluid} />
+              </Container>
+            ))}
+        </Carousel>
+      </>
+    )
+  }
+}
 
 export default EssayCarousel
--- src/components/2021/love-issue/Multimedia.js
+++ src/components/2021/love-issue/Multimedia.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import s from 'styled-components'
-import {FUTURA_REGULAR} from '../../../styles/font'
+import { FUTURA_REGULAR } from '../../../styles/font'
 import { Col, Row } from 'react-bootstrap'
 import Img from 'gatsby-image'
-import {Container, Tag, Author, LoveHeading, BoldText, RegularText} from '../../shared/index.js'
+import {
+  Container,
+  Tag,
+  Author,
+  LoveHeading,
+  BoldText,
+  RegularText
+} from '../../shared/index.js'
 import { useStaticQuery, graphql } from 'gatsby'
 
+// Nassim TODO: don't copy and paste the code everywhere, put
+// this into shared.js and pass in the colors
 const BoxedText = s.div`
   width: fit-content;
   padding: .5rem 1.2rem;
@@ -18,7 +27,14 @@ const BoxedText = s.div`
     margin-top: 4rem;
   }
 `
-
+// Nassim TODO:
+// 1. just put this entire graphql query inside love-issue.js and pass the content here
+// 2. i want to see less hardcoding of multimedia[0], multimedia[1], if it's possible to
+// do .map, just do that, u can get subarray through multimedia.slice(start index, end index)
+// 3. add a lil bit margin below the author name
+// 4. the description for each photo can be a lil bit larger
+// 5. the pink text can be a lil bit larger
+// 6. change the text to the correct ones
 const Multimedia = () => {
   const data = useStaticQuery(graphql`
     query {
@@ -26,17 +42,17 @@ const Multimedia = () => {
         edges {
           node {
             childrenLoveIssueJson {
-							multimedia{
-								author
-								link
-								title
-								img {
-									childImageSharp {
-										fluid(maxWidth: 1000) {
-											...GatsbyImageSharpFluid
-											src
-										}
-									}
+              multimedia {
+                author
+                link
+                title
+                img {
+                  childImageSharp {
+                    fluid(maxWidth: 1000) {
+                      ...GatsbyImageSharpFluid
+                      src
+                    }
+                  }
                 }
               }
             }
@@ -47,143 +63,185 @@ const Multimedia = () => {
   `)
 
   const {
-    node: {childrenLoveIssueJson: articles },
+    node: { childrenLoveIssueJson: articles }
   } = data.allFile.edges[0]
-	const {multimedia} = articles[1]
+  const { multimedia } = articles[1]
   return (
-		<>
-			<LoveHeading color='#FDB6B0'>{'MULTIMEDIA'}</LoveHeading>
-			<BoldText>VIDEO</BoldText>
-    	<RegularText style = {{marginBottom: "1rem"}}>
-      	It's Not You, It's COVID–19: Meet the Couples
-      	<br />
-      	Street sat down with two couples to learn more about their relationships,
-      	and how they had to change once Penn shut down last March.
-    	</RegularText>
-			<Row>
-				<Col sm={12} md={{ span: 3, offset: 2 }}>
-					<Row style={{marginBottom: "1.5rem"}}>
-						<Col sm={12} md={12}>
-							<Container>
-								<Tag>
-									<p style={{marginBottom: "0.2rem"}}>{multimedia[0].title}</p>
-									<Author>BY {multimedia[0].author}</Author>
-								</Tag>
-								<Img fluid={multimedia[0].img.childImageSharp.fluid}/>
-							</Container>
-						</Col>
-					</Row>
-					<Row style={{marginBottom: "1.5rem"}}>
-						<Col sm={12} md={12}>
-						<Container>
-								<Tag>
-									<p style={{marginBottom: "0.2rem"}}>{multimedia[1].title}</p>
-									<Author>BY {multimedia[1].author}</Author>
-								</Tag>
-								<Img fluid={multimedia[1].img.childImageSharp.fluid}/>
-							</Container>
-						</Col>
-					</Row>
-					<Row style={{marginBottom: "1.5rem"}}>
-						<Col sm={12} md={12}>
-						<Container>
-								<Tag>
-									<p style={{marginBottom: "0.2rem"}}>{multimedia[2].title}</p>
-									<Author>BY {multimedia[2].author}</Author>
-								</Tag>
-								<Img fluid={multimedia[2].img.childImageSharp.fluid} />
-							</Container>
-						</Col>
-					</Row>
-				</Col>
-				<Col sm={12} md={5}>
-					<Row style={{marginBottom: "1.5rem"}}>
-							<Col sm={12} md={12}>
-							<Container>
-									<Tag>
-										<p style={{marginBottom: "0.2rem"}}>{multimedia[3].title}</p>
-										<Author>BY {multimedia[3].author}</Author>
-									</Tag>
-									<Img fluid={multimedia[3].img.childImageSharp.fluid}/>
-								</Container>
-							</Col>
-						</Row>
-						<Row style={{marginBottom: "1.5rem"}}>
-							<Col sm={12} md={12}>
-							<Container>
-									<Tag>
-										<p style={{marginBottom: "0.2rem"}}>{multimedia[4].title}</p>
-										<Author>BY {multimedia[4].author}</Author>
-									</Tag>
-									<Img fluid={multimedia[4].img.childImageSharp.fluid}/>
-								</Container>
-							</Col>
-						</Row>
-						<Row style={{marginBottom: "1.5rem"}}>
-							<Col sm={12} md={12}>
-							<Container>
-									<Tag>
-										<p style={{marginBottom: "0.2rem"}}>{multimedia[5].title}</p>
-										<Author>BY {multimedia[5].author}</Author>
-									</Tag>
-									<Img fluid={multimedia[5].img.childImageSharp.fluid}/>
-								</Container>
-							</Col>
-						</Row>
-				</Col>
-			</Row>
-			<Row style={{marginBottom: "1.5rem"}}>
-				<Col md={2} style={{marginRight: "-4.2%", paddingRight: "5.5%"}}></Col>
-				<Col sm={12} md={3}>
-					<Container>
-						<Tag>
-							<p style={{marginBottom: "0.2rem"}}>{multimedia[6].title}</p>
-							<Author>BY {multimedia[6].author}</Author>
-						</Tag>
-						<Img fluid={multimedia[6].img.childImageSharp.fluid} style={{height: "500px"}}/>
-					</Container>
-				</Col>
-				<Col sm={12} md={3}>
-					<Container>
-						<Tag>
-							<p style={{marginBottom: "0.2rem"}}>{multimedia[7].title}</p>
-							<Author>BY {multimedia[7].author}</Author>
-						</Tag>
-						<Img fluid={multimedia[7].img.childImageSharp.fluid} style={{height: "500px"}}/>
-					</Container>
-				</Col>
-				<Col sm={12} md={3}>
-					<Container>
-						<Tag>
-							<p style={{marginBottom: "0.2rem"}}>{multimedia[8].title}</p>
-							<Author>BY {multimedia[8].author}</Author>
-						</Tag>
-						<Img fluid={multimedia[8].img.childImageSharp.fluid} style={{height: "500px"}}/>
-					</Container>
-				</Col>
-			</Row>
-			<Row >
-				<Col md={2} style={{marginRight: "-4.2%", paddingRight: "5.5%"}}></Col>
-				<Col sm={12} md={4} style={{marginBottom: "3rem"}}>	
-					<Container>
-						<Tag>
-							<p style={{marginBottom: "0.2rem"}}>{multimedia[9].title}</p>
-							<Author>BY {multimedia[9].author}</Author>
-						</Tag>
-						<Img fluid={multimedia[9].img.childImageSharp.fluid}/>
-					</Container>
-				</Col>
-				<Col sm={12} md={5} style={{marginBottom: "3rem", paddingTop: "2.5%"}}>	
-					<BoxedText>
-						<p>While none of us could have anticipated the effects that the advent of COVID-19 would have on our society, the hardships and grief of life under lockdown have certainly been felt by all of us. Seperated from our loved ones and facing a future full of uncertainty, quarantine has pushed us to find new ways to cope with our so-called "new normal".</p>
-						<p>Street welcomed staffers to submtit photos of the things that brought them a sense of peace during this time. From snuggling with pets to exploring a new hobby, here are a few tidbits of positivity that helped our staff survive the past year.</p>
-					</BoxedText>
-				</Col>
-			</Row>
-
-		</>
+    <>
+      <LoveHeading color="#FDB6B0">{'MULTIMEDIA'}</LoveHeading>
+      <BoldText>VIDEO</BoldText>
+      <RegularText style={{ marginBottom: '1rem' }}>
+        It's Not You, It's COVID–19: Meet the Couples
+        <br />
+        Street sat down with two couples to learn more about their
+        relationships, and how they had to change once Penn shut down last
+        March.
+      </RegularText>
+      <Row>
+        <Col sm={12} md={{ span: 3, offset: 2 }}>
+          <Row style={{ marginBottom: '1.5rem' }}>
+            <Col sm={12} md={12}>
+              <Container>
+                <Tag>
+                  <p style={{ marginBottom: '0.2rem' }}>
+                    {multimedia[0].title}
+                  </p>
+                  <Author>BY {multimedia[0].author}</Author>
+                </Tag>
+                <Img fluid={multimedia[0].img.childImageSharp.fluid} />
+              </Container>
+            </Col>
+          </Row>
+          <Row style={{ marginBottom: '1.5rem' }}>
+            <Col sm={12} md={12}>
+              <Container>
+                <Tag>
+                  <p style={{ marginBottom: '0.2rem' }}>
+                    {multimedia[1].title}
+                  </p>
+                  <Author>BY {multimedia[1].author}</Author>
+                </Tag>
+                <Img fluid={multimedia[1].img.childImageSharp.fluid} />
+              </Container>
+            </Col>
+          </Row>
+          <Row style={{ marginBottom: '1.5rem' }}>
+            <Col sm={12} md={12}>
+              <Container>
+                <Tag>
+                  <p style={{ marginBottom: '0.2rem' }}>
+                    {multimedia[2].title}
+                  </p>
+                  <Author>BY {multimedia[2].author}</Author>
+                </Tag>
+                <Img fluid={multimedia[2].img.childImageSharp.fluid} />
+              </Container>
+            </Col>
+          </Row>
+        </Col>
+        <Col sm={12} md={5}>
+          <Row style={{ marginBottom: '1.5rem' }}>
+            <Col sm={12} md={12}>
+              <Container>
+                <Tag>
+                  <p style={{ marginBottom: '0.2rem' }}>
+                    {multimedia[3].title}
+                  </p>
+                  <Author>BY {multimedia[3].author}</Author>
+                </Tag>
+                <Img fluid={multimedia[3].img.childImageSharp.fluid} />
+              </Container>
+            </Col>
+          </Row>
+          <Row style={{ marginBottom: '1.5rem' }}>
+            <Col sm={12} md={12}>
+              <Container>
+                <Tag>
+                  <p style={{ marginBottom: '0.2rem' }}>
+                    {multimedia[4].title}
+                  </p>
+                  <Author>BY {multimedia[4].author}</Author>
+                </Tag>
+                <Img fluid={multimedia[4].img.childImageSharp.fluid} />
+              </Container>
+            </Col>
+          </Row>
+          <Row style={{ marginBottom: '1.5rem' }}>
+            <Col sm={12} md={12}>
+              <Container>
+                <Tag>
+                  <p style={{ marginBottom: '0.2rem' }}>
+                    {multimedia[5].title}
+                  </p>
+                  <Author>BY {multimedia[5].author}</Author>
+                </Tag>
+                <Img fluid={multimedia[5].img.childImageSharp.fluid} />
+              </Container>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+      <Row style={{ marginBottom: '1.5rem' }}>
+        <Col
+          md={2}
+          style={{ marginRight: '-4.2%', paddingRight: '5.5%' }}
+        ></Col>
+        <Col sm={12} md={3}>
+          <Container>
+            <Tag>
+              <p style={{ marginBottom: '0.2rem' }}>{multimedia[6].title}</p>
+              <Author>BY {multimedia[6].author}</Author>
+            </Tag>
+            <Img
+              fluid={multimedia[6].img.childImageSharp.fluid}
+              style={{ height: '500px' }}
+            />
+          </Container>
+        </Col>
+        <Col sm={12} md={3}>
+          <Container>
+            <Tag>
+              <p style={{ marginBottom: '0.2rem' }}>{multimedia[7].title}</p>
+              <Author>BY {multimedia[7].author}</Author>
+            </Tag>
+            <Img
+              fluid={multimedia[7].img.childImageSharp.fluid}
+              style={{ height: '500px' }}
+            />
+          </Container>
+        </Col>
+        <Col sm={12} md={3}>
+          <Container>
+            <Tag>
+              <p style={{ marginBottom: '0.2rem' }}>{multimedia[8].title}</p>
+              <Author>BY {multimedia[8].author}</Author>
+            </Tag>
+            <Img
+              fluid={multimedia[8].img.childImageSharp.fluid}
+              style={{ height: '500px' }}
+            />
+          </Container>
+        </Col>
+      </Row>
+      <Row>
+        <Col
+          md={2}
+          style={{ marginRight: '-4.2%', paddingRight: '5.5%' }}
+        ></Col>
+        <Col sm={12} md={4} style={{ marginBottom: '3rem' }}>
+          <Container>
+            <Tag>
+              <p style={{ marginBottom: '0.2rem' }}>{multimedia[9].title}</p>
+              <Author>BY {multimedia[9].author}</Author>
+            </Tag>
+            <Img fluid={multimedia[9].img.childImageSharp.fluid} />
+          </Container>
+        </Col>
+        <Col
+          sm={12}
+          md={5}
+          style={{ marginBottom: '3rem', paddingTop: '2.5%' }}
+        >
+          <BoxedText>
+            <p>
+              While none of us could have anticipated the effects that the
+              advent of COVID-19 would have on our society, the hardships and
+              grief of life under lockdown have certainly been felt by all of
+              us. Seperated from our loved ones and facing a future full of
+              uncertainty, quarantine has pushed us to find new ways to cope
+              with our so-called "new normal".
+            </p>
+            <p>
+              Street welcomed staffers to submtit photos of the things that
+              brought them a sense of peace during this time. From snuggling
+              with pets to exploring a new hobby, here are a few tidbits of
+              positivity that helped our staff survive the past year.
+            </p>
+          </BoxedText>
+        </Col>
+      </Row>
+    </>
   )
 }
-
 
 export default Multimedia

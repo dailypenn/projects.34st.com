ade58b259b05999c4f3582aa3e3bd5c7f28d9054
andy jiang <andyjiang55@yahoo.com>
2021-03-23 21:10:58 -0400

    start

---
+++ src/components/2021/dining-guide-spring/Instagram.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import s from 'styled-components'
+import InstagramEmbed from 'react-instagram-embed'
+
+const Container = s.div`
+  width: 100%;
+  padding: 3rem;
+
+  @media(max-width: 768px) {
+    padding: 3rem 2rem;
+  }
+`
+
+const Instagram = () => {
+  <Container> 
+    
+  </Container>
+}

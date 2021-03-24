5f3fd1d5dc7ad020df1cdcab33f0f1442fcb4e57
Raunaq Singh <raunaqsingh2020@gmail.com>
2021-03-24 01:01:42 -0400

    fix json graphql issues

--- src/pages/2021/pre-med-humanities-covid-19-pandemic-medicine-penn.js
+++ src/pages/2021/pre-med-humanities-covid-19-pandemic-medicine-penn.js
@@ -138,7 +138,14 @@ const Index = () => {
                 content
               }
               social {
-                img
+                img {
+                  childImageSharp {
+                    fluid(maxWidth: 1000, maxHeight: 900) {
+                      ...GatsbyImageSharpFluid
+                      src
+                    }
+                  }
+                }
                 slug
               }
             }

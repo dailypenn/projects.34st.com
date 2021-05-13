7879781ece512b884de3d0d5a1d4a2a9eccbaed1
Raunaq Singh <raunaqsingh2020@gmail.com>
2021-05-13 01:20:35 -0400

    Updates

---
+++ component---src-pages-404-js-6c74347836b74ef0ce38.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{w2l6:function(n,t,o){"use strict";o.r(t);var i=o("q1tI"),c=o("Wbzz");t.default=function(){return Object(i.useEffect)((function(){var n=window.location.href.split("/",3).join("/").length,t=window.location.href.substring(n);Object(c.c)("https://dailypenn.github.io/old.projects.34st.com"+t)}),[]),null}}}]);
+//# sourceMappingURL=component---src-pages-404-js-6c74347836b74ef0ce38.js.map
